fa4b1a647ff4ca46a11dbb5a881f36b15637ae11
karanssu <s0377009salemstate@gmail.com>
2024-11-30 01:27:25 -0500

    Hire me button styled and responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div className="md:flex h-screen bg-gray-800 text-gray-100 font-sans p-5">
 			<div className="md:flex-col w-full md:p-10">
-				<div className="mb-5 md:mb-20">
+				<div className="mb-8 md:mb-10">
 					<div className="text-5xl mb-2 md:text-7xl font-semibold ">
 						Karan Patel
 					</div>
@@ -17,7 +17,17 @@ export default function Home() {
 					</div>
 				</div>
 
-				<div className="hidden md:block text-xl mb-10">
+				<div className="mb-10 md:mb-20">
+					<a
+						className="py-2 px-8 bg-gray-100 text-gray-800 text-lg font-bold rounded-full p-1 cursor-pointer"
+						href="mailto:karanpatel4182@gmail.com"
+						target="_blank"
+					>
+						Hire Me
+					</a>
+				</div>
+
+				<div className="hidden md:block text-xl mb-40">
 					<div className={`text-white flex`}>
 						<div className="mr-2">----</div>
 						<div>About</div>
@@ -25,41 +35,37 @@ export default function Home() {
 					<div className={`text-gray-400`}>Projects</div>
 				</div>
 
-				<div className="mb-5 md:mb-40">
-					<div>Hire me</div>
-				</div>
-
 				<div className="flex mb-20 md:mb-0">
 					<a
-						className="mr-5 bg-gray-100 rounded-full p-1 cursor-pointer"
+						className="mr-8 bg-gray-100 rounded-full p-1 cursor-pointer"
 						href="https://github.com/karanssu"
 						target="_blank"
 					>
 						<Image
-							width={30}
-							height={30}
+							width={28}
+							height={28}
 							src={"github.svg"}
 							alt="Github Icon"
 						/>
 					</a>
 					<a
-						className="mr-5 bg-gray-100 rounded-full p-1 cursor-pointer"
+						className="mr-8 bg-gray-100 rounded-full p-1 cursor-pointer"
 						href="https://www.linkedin.com/in/karan-patel-58b193304/"
 						target="_blank"
 					>
 						<Image
-							width={30}
-							height={30}
+							width={28}
+							height={28}
 							src={"linkedin.svg"}
 							alt="Linkedin Icon"
 						/>
 					</a>
 					<a
-						className="mr-5 bg-gray-100 rounded-full p-1 cursor-pointer"
+						className="mr-8 bg-gray-100 rounded-full p-1 cursor-pointer"
 						href="mailto:karanpatel4182@gmail.com"
 						target="_blank"
 					>
-						<Image width={30} height={30} src={"mail.svg"} alt="Mail Icon" />
+						<Image width={28} height={28} src={"mail.svg"} alt="Mail Icon" />
 					</a>
 				</div>
 			</div>
